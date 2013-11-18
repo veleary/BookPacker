02ef9b014efa97ae29253033f62c4d6c7511beaa
veleary <veleary@gmail.com>
2013-11-18 16:50:41 -0600

    update with JSON formatting with required information

--- app/helpers/books_helper.rb
+++ app/helpers/books_helper.rb
@@ -2,7 +2,7 @@ module BooksHelper
   require 'json'
   def create_box(id, weight)
         
-    box = {"id" => id, "Total_Weight" => weight, "Book_list" => []}
+    box = {"box" => {"id" => id, "total_weight" => weight, "contents" => []}}
     @boxes << box      
     pack_books
   end
@@ -19,13 +19,25 @@ module BooksHelper
   def pack_books       
     @weights.each_with_index do  |weight, index|
       box = @boxes.last
-      sum = weight.to_f + box["Total_Weight"]
+      sum = weight.to_f + box["box"]["total_weight"]
       if  sum <= 10
-        box["Total_Weight"] += weight.to_f
-        box["Book_list"] <<  Book.find_by_ship_weight(weight).title
+        box["box"]["total_weight"] += weight.to_f
+        book = Book.find_by_ship_weight(weight)
+        content_info = {
+          "title:" => book.title, 
+          "author:" => book.author,       
+          "price:" => book.price,
+          "ship_weight:" => book.ship_weight,
+          "isbn10:" => book.isbn10,
+          "isbn13:" => book.isbn13,
+          "total_pages:" => book.total_pages,
+          "publisher:" => book.publisher,
+          "language:" => book.language
+        }
+        box["box"]["contents"] << content_info
         @weights.delete_at(index)
       else
-        new_box_id = box["id"] + 1
+        new_box_id = box["box"]["id"] + 1
         create_box(new_box_id, 0)       
       end
     end
@@ -33,12 +45,6 @@ module BooksHelper
 
 
   def packing_list
-    return JSON.pretty_generate(@boxes).html_safe;
-    # return @boxes
-      # Box_Number = box[id] 
-
-    # return @boxes.last
-    # return @weights
-    # return @box_list
+    JSON.pretty_generate(@boxes).html_safe;    
   end
 end
